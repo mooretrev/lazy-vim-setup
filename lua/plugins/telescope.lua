31b3b4c6de536fb3445d5ca0eb8a7b02bbae62da
Trevor Moore <tmoore01@pros.com>
2024-01-03 01:47:16 -0600

    Refactored java config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,8 @@ return {
       { "<leader>fo", "<cmd> Telescope oldfiles <CR>", desc = "Find oldfiles" },
       { "<leader>ff", "<cmd> Telescope find_files <CR>", desc = "Find all files" },
       { "<leader>fb", "<cmd> Telescope git_branches <CR>", desc = "Find git branches" },
+      { "<leader>fs", "<cmd> Telescope git_status <CR>", desc = "Find git files changes" },
+      { "<leader>gs", false},
       { "<leader>/", false },
     },
   },
