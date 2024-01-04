a815cca62bd343270148c7968e8065cb3285d980
Trevor Moore <tmoore01@pros.com>
2024-01-04 09:05:23 -0600

    Change to live grep for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     "nvim-telescope/telescope.nvim",
     keys = {
       -- disable the keymap to grep files
-      { "<leader>fg", "<cmd>Telescope grep_string<cr>", desc = "Grep (Find All)" },
+      { "<leader>fg", "<cmd>Telescope live_grep <cr>", desc = "Grep (Find All)" },
       { "<leader>fo", "<cmd> Telescope oldfiles <CR>", desc = "Find oldfiles" },
       { "<leader>ff", "<cmd> Telescope find_files <CR>", desc = "Find all files" },
       { "<leader>fb", "<cmd> Telescope git_branches <CR>", desc = "Find git branches" },
