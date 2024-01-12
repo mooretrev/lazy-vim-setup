6ccbaee9a782237b09a78fcaac889783c8e1ef99
Trevor Moore <tmoore01@pros.com>
2024-01-12 11:34:51 -0600

    Show hidden file in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,12 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
-  -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     { "-", "<cmd>Oil <CR>", desc = "Open parent directory" },
   },
+  config = function()
+    require("oil").setup({ view_options = {
+      show_hidden = true,
+    } })
+  end,
 }
