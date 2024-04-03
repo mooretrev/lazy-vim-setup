152654f4deb66c16f2951f7ea9529ab9480f29a3
Trevor Moore <tmoore01@pros.com>
2024-04-03 15:00:34 -0500

    Cucumber language server

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,10 +1,27 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    autoformat = false,
     servers = {
-      jdtls = {
-        settings = {},
+      solargraph = {},
+      cucumber_language_server = {
+        settings = {
+          features = { "cucumber-tests/**/*.feature" },
+          glue = { "cucumber-tests/**/*.rb", "/opt/homebrew/lib/ruby/gems/2.7.0/gems/json_spec-1.1.4/**/*.rb" },
+          parameterTypes = {},
+          snippetTemplates = {},
+        },
+        handlers = {
+          ["workspace/configuration"] = function()
+            return {
+              {
+                features = { "cucumber-tests/**/*.feature" },
+                glue = { "cucumber-tests/**/*.rb", "/opt/homebrew/lib/ruby/gems/2.7.0/gems/json_spec-1.1.4/**/*.rb" },
+                parameterTypes = {},
+                snippetTemplates = {},
+              },
+            }
+          end,
+        },
       },
     },
   },
